e1ed408bffd9f0159db6eb6104cd372ae07ea4f1
JiseonYu <49763204+JiseonYu@users.noreply.github.com>
2020-01-21 15:17:56 -0500

    Add events.py

--- whatsapp-twilio/events.py
+++ whatsapp-twilio/events.py
@@ -1,10 +1,34 @@
 import requests
-        
-def get_events_data():
-    quote_page = 'https://www1.nyc.gov/calendar/api/json/search.htm?sort=DATE&pageNumber=1'
-    page = requests.get(url=quote_page).json()
-    items = page['items']
-    print(items)
-    return items
+import time
 
-get_events_data()
+data_dict={
+
+}
+
+def update_all_events_data():
+    page=requests.get("https://www1.nyc.gov/calendar/api/json/search.htm?sort=DATE&pageNumber=1").json()
+    num_pages = page['pagination']['numPages']
+    events_array = page['items']
+
+    for i in range(2, num_pages + 1):
+        new_page = requests.get("https://www1.nyc.gov/calendar/api/json/search.htm?sort=DATE&pageNumber={}".format(i)).json()
+        new_items = new_page['items']
+        events_array += new_items
+
+    return events_array
+
+def get_all_events_data():
+    currentTime=time.time()
+    if 'events' not in data_dict:
+        data_dict['events'] = {'time':None, 'data':None}
+    lastTime=data_dict['events']['time']
+    if lastTime == None or lastTime + 3600000 < currentTime:
+        print('getting data')
+        data = update_all_events_data()
+
+        data_dict['events']['time']=currentTime
+        data_dict['events']['data']=data
+
+    print('returned data')
+    return data_dict['events']['data']
+        
