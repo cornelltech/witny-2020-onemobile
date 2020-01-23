ddda59cdfc9b6bf559390c7619a2a04dd4abed54
JiseonYu <49763204+JiseonYu@users.noreply.github.com>
2020-01-23 15:42:50 -0500

    Update events.py

--- whatsapp-twilio/events.py
+++ whatsapp-twilio/events.py
@@ -23,11 +23,56 @@ def get_all_events_data():
         data_dict['events'] = {'time':None, 'data':None}
     lastTime=data_dict['events']['time']
     if lastTime == None or lastTime + 3600000 < currentTime:
+        #print('getting data')
         data = update_all_events_data()
 
         data_dict['events']['time']=currentTime
         data_dict['events']['data']=data
 
-    
+    #print('returned data')
     return data_dict['events']['data']
-        
+
+
+def group_by_boroughs():
+    event_array=get_all_events_data()
+    count=len(event_array) #552
+         
+    queens = [] #Qn
+    manhattan=[] #Mn
+    bronx=[] #Bx
+    brooklyn=[] #Bk
+    sIsland=[] #Si
+
+    for i in range(count):
+        boroughs = event_array[i]['boroughs']
+        if 'Mn' in boroughs:
+            manhattan.append(event_array[i])
+        elif 'Qn' in boroughs:
+            queens.append(event_array[i])
+        elif 'Bx' in boroughs:
+            bronx.append(event_array[i])
+        elif 'Bk' in boroughs:
+            brooklyn.append(event_array[i])
+        elif 'Si' in boroughs:
+            sIsland.append(event_array[i])
+
+   
+    data_dict['events']['manhattan'] = manhattan
+    data_dict['events']['queens'] =  queens
+    data_dict['events']['bronx'] = bronx
+    data_dict['events']['brooklyn'] = brooklyn
+    data_dict['events']['sIsland'] = sIsland
+
+
+def get_events_boroughs(boroughs):
+    
+    if boroughs == 'manhattan':
+        return data_dict['events']['manhattan']
+    elif boroughs == 'brooklyn':
+        return data_dict['events']['brooklyn']
+    elif boroughs == 'bronx':
+        return data_dict['events']['bronx']
+    elif boroughs == 'queens':
+        return data_dict['events']['queens']
+    elif boroughs == 'staten island':
+        return data_dict['events']['sIsland']
